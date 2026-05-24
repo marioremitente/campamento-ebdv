a9837f55a58272ed7a1ae2f08be2cfb36bc4af13
MarioRemitente <99061361+marioremitente@users.noreply.github.com>
2026-05-24 18:17:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,13 @@
-const CACHE_NAME = 'campamento-ebdv-v1';
-
-const urlsToCache = [
-  './',
-  './login.html',
-  './consulta.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE_NAME = 'campamento-ebdv-v3';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+  event.respondWith(fetch(event.request));
 });
