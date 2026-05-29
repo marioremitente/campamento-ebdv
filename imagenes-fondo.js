0e5fc8873ed8c37f75054fc49c7f071c20b62661
MarioRemitente <99061361+marioremitente@users.noreply.github.com>
2026-05-29 13:17:57 -0400

    Add files via upload

--- imagenes-fondo.js
+++ imagenes-fondo.js
@@ -1,17 +1,5 @@
-// imagenes-fondo.js
-// Fondo centrado y ampliado ocupando toda la pantalla
-
-(function () {
-
-  document.addEventListener("DOMContentLoaded", function () {
-
-    document.body.style.backgroundImage = "url('1(1).png')";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundPosition = "center center";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundAttachment = "fixed";
-    document.body.style.minHeight = "100vh";
-
-  });
-
-})();
+document.body.style.backgroundImage = "url('(1).png')";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundPosition = "center";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.minHeight = "100vh";
