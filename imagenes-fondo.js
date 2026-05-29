59ad28173e3b88e236aa3d113557179abcd5dbf1
MarioRemitente <99061361+marioremitente@users.noreply.github.com>
2026-05-29 13:19:45 -0400

    Update imagenes-fondo.js

--- imagenes-fondo.js
+++ imagenes-fondo.js
@@ -1,5 +1,19 @@
-document.body.style.backgroundImage = "url('(1).png')";
+const imagenes = [
+  "(1).png",
+  "(2).png",
+  "(3).png",
+  "(4).png",
+  "(5).png",
+  "(6).png"
+];
+
+// Escoge una imagen aleatoria
+const imagenAleatoria =
+  imagenes[Math.floor(Math.random() * imagenes.length)];
+
+document.body.style.backgroundImage = `url('${imagenAleatoria}')`;
 document.body.style.backgroundSize = "cover";
 document.body.style.backgroundPosition = "center";
 document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundAttachment = "fixed";
 document.body.style.minHeight = "100vh";
